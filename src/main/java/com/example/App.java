046a24745e646687c054cc3eea259faf6fda2311
hari170998 <harivishnu170998@gmail.com>
2026-03-12 15:18:57 +0530

    first commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,12 @@
+#java app
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello welcome, CI/CD World!");
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
